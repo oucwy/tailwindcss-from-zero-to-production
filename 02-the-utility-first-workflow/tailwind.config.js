5f033c9e36a118f01cae4d87d2a031a96e52ffee
oucwy <wangyong@ouc.edu.cn>
2024-02-16 22:54:43 +0800

    markwy 2024-2-16

--- 02-the-utility-first-workflow/tailwind.config.js
+++ 02-the-utility-first-workflow/tailwind.config.js
@@ -1,11 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+  plugins: [
+
+  ],
+}
+
